eacc55f24ce510f2e86ee0b0d62090f67c074402
Csaba Gyuris <gycsaba@vasvari.hu>
2019-03-04 14:53:51 +0100

    Pekingi torna feladat

---
+++ OOPObjectInList - PekingiTorna/repository/PekingiTornaszTorna.cs
@@ -0,0 +1,21 @@
+﻿using OOPObjectInList___PekingiTorna.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPObjectInList___PekingiTorna.repository
+{
+    class PekingiTornaszTorna
+    {
+        List<Versenyzo> versenyzok;
+
+        public PekingiTornaszTorna()
+        {
+            versenyzok = new List<Versenyzo>();
+
+            versenyzok.Add(new Versenyzo(178, "YANG Wei", "CHN", "Ázsia" , 15.400, 15.425, 16.225,16.550, 15.350, 14.925));
+        }
+    }
+}
