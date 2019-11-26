ddf6eac700f5bf97b79cf4133aadce0de52bf2bf
Csaba Gyuris <gycsaba@vasvari.hu>
2019-11-26 10:40:34 +0100

    Repo és modell kész, adatok beolvasva

--- Vitorlaverseny/Program.cs
+++ Vitorlaverseny/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vitorlaverseny.tarolo;
 
 namespace Vitorlaverseny
 {
@@ -10,6 +11,10 @@ namespace Vitorlaverseny
     {
         static void Main(string[] args)
         {
+            Tarolo t = new Tarolo();
+            t.beolvas();
+            Console.WriteLine("Versenyzők száma: "+t.getVersenyzokSzama());
+            Console.ReadKey();
         }
     }
 }
